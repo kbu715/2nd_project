b79bdae603180642fe0ccf3e99ae15f889d58b9c
kbu715 <kbu715@gmail.com>
2020-09-18 23:13:53 +0900

    login with firebase

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
   const onChange = (event) => {
       const { target: { name, value } } = event;
 
@@ -12,8 +15,27 @@ const Auth = () => {
           setPassword(value);
       }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    let data;
+    try {
+      if(newAccount) {
+        //create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        )
+      } else {
+        //log in
+        data = await authService.signInWithEmailAndPassword(
+          email,
+          password
+        )
+      }
+      console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
   };
   return (
     <div>
@@ -34,7 +56,7 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
