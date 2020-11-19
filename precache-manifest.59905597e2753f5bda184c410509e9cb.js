ffcd5fe40419539a1f0d0d6849e63c0c90eae63f
kbu715 <kbu715@gmail.com>
2020-11-19 17:29:16 +0900

    Updates

---
+++ precache-manifest.59905597e2753f5bda184c410509e9cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc8a92134b8205ec3f12b83f3420b1b",
+    "url": "/2nd_project/index.html"
+  },
+  {
+    "revision": "f376f3cf30d614186cb3",
+    "url": "/2nd_project/static/css/main.5562a246.chunk.css"
+  },
+  {
+    "revision": "4609a2d4826def2573a7",
+    "url": "/2nd_project/static/js/2.55180381.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/2nd_project/static/js/2.55180381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f376f3cf30d614186cb3",
+    "url": "/2nd_project/static/js/main.26fb839f.chunk.js"
+  },
+  {
+    "revision": "e8e4e8308ff2c30ccbb7",
+    "url": "/2nd_project/static/js/runtime-main.00e83f53.js"
+  }
+]);
